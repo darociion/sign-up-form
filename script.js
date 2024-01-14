025703ef8cc68ce1396a0c2db991ec6cd05272cd
Jon Gomez <90850607+darociion@users.noreply.github.com>
2024-01-14 04:59:27 -0300

    Some changes

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-let pass = document.getElementById("pass");
-let confirmpass = document.getElementById("confirmpass");
-let errorMessage = document.querySelector(".error-message");
+let pass = document.querySelector("#pass");
+let confirmPass = document.querySelector("#confirmPass");
+let errorMessage = document.querySelector(".errorMessage");
 
-confirmpass.addEventListener("input", function (event) {
-    if (pass.value !== confirmpass.value) {
-        errorMessage.innerHTML = "*Passwords do not match";
+confirmPass.addEventListener("input", function (event) {
+    if (pass.value !== confirmPass.value) {
+        errorMessage.style.visibility = "visible";
         pass.classList.add("error");
-        confirmpass.style.border = "1px solid red";
+        confirmPass.style.border = "1px solid red";
     } else {
-        errorMessage.innerHTML = "";
+        errorMessage.style.visibility = "hidden";
         pass.classList.remove("error");
-        confirmpass.style.border = "rgb(5, 5, 141) 1px solid";
-        confirmpass.style.boxShadow = "2px 5px 8px 0 rgb(0, 0, 0, 0.2)";
+        confirmPass.style.border = "rgb(5, 5, 141) 1px solid";
+        confirmPass.style.boxShadow = "2px 5px 8px 0 rgb(0, 0, 0, 0.2)";
     } 
     }
 )
